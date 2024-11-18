2207ae36866e14b849c6be6b0e5ef1bed5895742
Joaliny Furtado <Joalinyfurtado87@gmail.com>
2024-11-18 11:03:00 -0400

    criado mais uma models unidade

--- app/Models/Sala.php
+++ app/Models/Sala.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Reserva;
 
 
 class Sala extends Model
 {
     use HasFactory;
     protected $fillable =['nome', 'descricao'];    
+
+
+    public function Reserva()
+    {
+        return $this->hasMany(Reserva::class,'reserva_fk','id');
+    }
+
 }
---
+++ app/Models/Unidade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Unidade extends Model
+{
+    use HasFactory;
+
+        protected $fillable =['nome', 'sigla'];
+
+        public function User()
+        {
+            return $this->hasMany(User::class, 'user_id', 'id ');
+        }
+
+
+}
