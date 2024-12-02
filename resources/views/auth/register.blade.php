881c2e32b11671866abd92545470e7cb54f15b04
Leo S. <contato.prof.leonardo@gmail.com>
2024-12-02 16:03:00 -0400

    Página de registro PRONTA

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  
   <link rel="stylesheet" href="{{ asset('css/login.css') }}">
   <title>Document</title>
 </head>
@@ -29,36 +33,71 @@
             <!-- Nome -->
             <div class="mb-3 form-box">
               <label for="name">Nome completo:</label>
-              <input type="text" id="name" name="name" class="form-control" :value="old('name')" required autofocus placeholder="Julliany Souza" autocomplete="name">
+
+              <div class="input-group">
+                <span class="input-group-text">
+                  <i class="bi bi-person"></i>
+                </span>
+
+                <input type="text" id="name" name="name" class="form-control" :value="old('name')" required autofocus placeholder="ex: Julliany Souza" autocomplete="name">
+              </div>
 
               <x-input-error :messages="$errors->get('name')" class="mt-2" />
             </div>
-            
+
             <!-- Email -->
             <div class="mb-3 form-box">
               <label for="email">Email:</label>
-              <input type="email" id="email" name="email" class="form-control" :value="old('email')" required placeholder="meuemail@email.com" autocomplete="username">
+              
+              <div class="input-group">
+                <span class="input-group-text">
+                  <i class="bi bi-envelope-at"></i>
+                </span>
 
+                <input type="email" id="email" name="email" class="form-control" :value="old('email')" required placeholder="ex: meuemail@email.com" autocomplete="username">
+              </div>
+              
               <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
             <!-- CPF -->
             <div class="mb-3 form-box">
               <label for="cpf">CPF:</label>
-              <input type="text" id="cpf" name="cpf" class="form-control" required placeholder="000.000.000-00">
+
+              <div class="input-group">
+                <span class="input-group-text">
+                  <i class="bi-person-vcard"></i>
+                </span>
+
+                <input type="text" id="cpf" name="cpf" class="form-control" required placeholder="ex: 000.000.000-00">
+              </div>
             </div>
 
             <!-- Senha -->
             <div class="mb-3 form-box">
               <label for="password">Senha:</label>
-              <input type="password" id="password" name="password" class="form-control" required placeholder="Mínimo de 8 caracteres" autocomplete="new-password">
 
+              <div class="input-group">
+                <span class="input-group-text">
+                  <i class="bi bi-shield-lock"></i>
+                </span>
+
+                <input type="password" id="password" name="password" class="form-control" required placeholder="Mínimo de 8 caracteres" autocomplete="new-password">
+              </div>
+              
               <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
 
             <div class="mb-3 form-box">
               <label for="password_confirmation">Repita a senha:</label>
-              <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required placeholder="Repita a senha" autocomplete="new-password">
+
+              <div class="input-group">
+                <span class="input-group-text">
+                  <i class="bi bi-shield-lock"></i>
+                </span>
+
+                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required placeholder="Repita a senha" autocomplete="new-password">
+              </div>
 
               <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
