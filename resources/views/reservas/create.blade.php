7fc3f69186c294fb71bb0377e36fe7a4568864f2
Leo S. <contato.prof.leonardo@gmail.com>
2024-12-05 16:26:01 -0400

    Estilização reservas

--- resources/views/reservas/create.blade.php
+++ resources/views/reservas/create.blade.php
@@ -1,32 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
 
-  <h1>Cadastro de Sala</h1>
-  
-  <form action="{{ route('salas.store') }}" method="POST">
-    @csrf
-    <div class="form-group">
-      <label for="nome">Sala</label>
-      <input type="text" name="nome" id="nome" class="form-control" required>
-    </div>
+<style>
+  /* Centralizar apenas o container */
+  .content-wrapper {
+    display: flex;
+    justify-content: center; /* Centraliza horizontalmente */
+    align-items: center;    /* Centraliza verticalmente */
+    min-height: 80vh;      /* Garante que o pai ocupe toda a altura da tela */
+  }
 
-    <div class="form-group">
-      <label for="descricao">Descrição/Localização</label>
-      <input type="text" name="descricao" id="descricao" class="form-control" required>
-    </div>
+  .container {
+    max-width: 720px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.10);
+    padding: 15px;
+    background-color: #f8fafc;
+    border-radius: 8px; /* Bordas arredondadas */
+  }
+</style>
 
-    <div class="form-group">
-      <label for="situacao">Situação</label>
-      <select name="situacao" id="situacao" class="form-control" required>
-        <option value="ativa">Ativa</option>
-        <option value="inativa">Inativa</option>
-      </select>
-    </div>
+<div class="content-wrapper">
+  <div class="container">
 
-    <button type="submit" class="btn btn-success">Salvar</button>
+    <h3 class="text-center">Cadastro de Sala</h3>
 
-  </form>
+    <form action="{{ route('salas.store') }}" method="POST">
+      @csrf
+
+      <div class="form-group mb-3">
+        <label for="nome" class="mb-1">Sala:</label>
+        <input type="text" name="nome" id="nome" class="form-control" required>
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="descricao">Descrição/Localização:</label>
+        <input type="text" name="descricao" id="descricao" class="form-control" required>
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="situacao">Situação:</label>
+        <select name="situacao" id="situacao" class="form-select" required>
+          <option value="ativa">Ativa</option>
+          <option value="inativa">Inativa</option>
+        </select>
+      </div>
+
+      <button type="submit" class="btn btn-success">Salvar</button>
+
+    </form>
+  </div>
 </div>
+
 @endsection
