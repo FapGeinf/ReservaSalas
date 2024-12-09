fee5d52bced832f54f9afb3216937fc49da29be4
Leo S. <contato.prof.leonardo@gmail.com>
2024-12-09 14:47:13 -0400

    Nova sidenav BASE

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,59 +1,146 @@
-<style>
-  li a:hover {
-    text-decoration: underline;
-  }
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Responsive Sidebar</title>
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
+</head>
 
-<div>
-  <nav class="navbar navbar-expand-lg" style="background: #245fa3; box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);">
-    <div class="container-fluid">
-      <img class="navbar-brand text-light" src="{{ asset('/img/logo-letras-white.png') }}" style="width: 6%;" alt="Logo Agendaí">
+<body>
+  <nav id="menu" class="menu">
+    <div class="actionBar">
+      <div>
+        <button id="menuBtn">
+          <i class="fa-solid fa-bars"></i>
+        </button>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+        <h3 class="menuText">Dashboard</h3>
+      </div>
+    </div>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav w-100 justify-content-center me-auto mb-2 mb-lg-0">
+    <ul class="optionsBar">
+      <li class="menuItem">
+        <a href="#" class="menuOption">
+          <i class="fa-solid fa-house"></i><h5 class="menuText">Home</h5>
+        </a>
+      </li>
 
-          <li class="nav-item">
-            <a class="nav-link fw-bold text-light" href="{{ route('home') }}">Início</a>
-          </li>
+      <li class="menuBreak">
+        <hr>
+      </li>
 
-          <li class="nav-item">
-            <a class="nav-link fw-bold text-light" href="{{ route('salas') }}">Salas</a>
-          </li>
-            
-          <li class="nav-item">
-            <a class="nav-link fw-bold text-light" href="{{ route('reservas.index') }}">Reservas</a>
-          </li>
-          
-        </ul>
+      <li class="menuItem">
+        <button id="productManagerBtn" class="menuOption">
+          <i class="fa-solid fa-shopping-bag"></i><h5 class="menuText">Product Manager</h5>
+        </button>
+      </li>
 
-        <li class="nav-item dropdown d-flex">
-          <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Conta
-          </a>
+      <li class="menuItem">
+        <button id="constantManagerBtn" class="menuOption">
+          <i class="fa-solid fa-border-all"></i><h5 class="menuText">Constant Manager</h5>
+        </button>
+      </li>
 
-          <ul class="dropdown-menu dropdown-menu-end">
-            <li>
-              <a class="dropdown-item" href="{{ route('profile.edit') }}">Editar Perfil</a>
-            </li>
+      <li class="menuItem">
+        <button id="orderManagerBtn" class="menuOption">
+          <i class="fa-solid fa-shopping-bag"></i><h5 class="menuText">Order Manager</h5>
+        </button>
+      </li>
 
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+      <li class="menuItem">
+        <button id="tagManagerBtn" class="menuOption">
+          <i class="fa-solid fa-tag"></i><h5 class="menuText">Tags Manager</h5>
+        </button>
+      </li>
+    </ul>
 
-            <li>
-              <form class="d-flex" method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button class="dropdown-item" type="submit">Sair</button>
-              </form>
-            </li>
+    <div class="menuUser">
+      <a href="#">
+        <div>
+          <img src="https://i.postimg.cc/5tPgPgyp/user.jpg" alt="image">
+        </div>
 
-          </ul>
-        </li>
+        <h5 class="username menuText">Alex</h5>
+        <p class="menuText"><i class="fa-solid fa-chevron-right"></i></p>
+      </a>
+
+      <div class="userInfo">
+        <div>
+          <h1><i class="fa-solid fa-exclamation-circle"></i></h1>
+          <p>User Info</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="themeBar">
+      <div>
+        <button id="themeChangeBtn"><i class="fa-solid "></i></button>
       </div>
     </div>
   </nav>
-</div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="main.js"></script>
+
+  <script>
+    const menuBtn = document.getElementById('menuBtn');
+    const menu = document.getElementById('menu');
+    const menuText = document.querySelectorAll('.menuText');
+
+    menuBtn.addEventListener('click', () => {
+      menu.classList.toggle('open');
+      menuText.forEach(function(text, index) {
+        setTimeout(() => {
+          text.classList.toggle('open2');
+        }, index * 50);
+      })
+    })
+
+    $(document).on('click', function(e) {
+      if($(e.target).closest('#menu').length === 0) {
+        menu.classList.toggle('open');
+        menuText.forEach(function(text, index) {
+          setTimeout(() => {
+            text.classList.toggle('open2');
+          }, index * 50);
+        })
+      }
+    })
+
+    const dayNight = document.querySelector('#themeChangeBtn');
+    dayNight.addEventListener('click', () => {
+      document.body.classList.toggle('dark');
+      if(document.body.classList.contains('dark')){
+        localStorage.setItem('theme', 'dark');
+      }else {
+        localStorage.setItem('theme','light');
+      }
+      updateIcon();
+    })
+
+    function themeMode() {
+      if(localStorage.getItem('theme') !== null){
+        if(localStorage.getItem('theme') === 'light'){
+          document.body.classList.remove('dark');
+        }else {
+          document.body.classList.add('dark');
+        }
+      }
+      updateIcon();
+    }
+    themeMode();
+
+    function updateIcon() {
+      if(document.body.classList.contains('dark')){
+        dayNight.querySelector('i').classList.remove('fa-moon');
+        dayNight.querySelector('i').classList.add('fa-sun');
+      } else {
+        dayNight.querySelector('i').classList.remove('fa-sun');
+        dayNight.querySelector('i').classList.add('fa-moon');
+      }
+    }
+  </script>
+</body>
+</html>
