9a7463747eed1b7c0b8f21b638ec48851b6361ed
Joaliny Furtado <Joalinyfurtado87@gmail.com>
2024-12-30 12:06:51 -0400

    trabalhando no calendar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Reserva;
+use App\Models\User;
+use App\Models\Sala;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        // // Busca todas as reservas de salas
-        // $reservas = Reserva::with('sala', 'usuario')->get();
 
-        // // Retorna a view 'home' com as reservas
-        // return view('home', compact('reservas'));
-        return view('home'); 
+
+        {
+            $events = [];
+     
+            $reservas = Reserva::with(['sala'])->get();
+     
+            foreach ($reservas as $reserva) {
+                $events[] = [
+                    'title' => $reserva->sala->name . '('.$reserva->sala->name.')',
+                    'start' => $reserva->data_inicio,
+                    'end' => $reserva->data_fim,
+                ];
+            }
+            return view('home', compact('events'));
+        }
     }
 }
-
--- resources/views/reservas/index.blade.php
+++ resources/views/reservas/index.blade.php
@@ -1,8 +1,26 @@
+@push('scripts')
+<link href="https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid/main.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
+        <script> 
+            document.addEventListener('DOMContentLoaded', function () {
+                var calendarEl = document.getElementById('calendar');
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    initialView: 'timeGridWeek',
+                    slotMinTime: '8:00:00',
+                    slotMaxTime: '19:00:00',
+                    
+                });
+                calendar.render();
+            });
+        </script>
+    @endpush
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-   
+<div id="calendar"></div>
 
     <!-- Botão para abrir o modal -->
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#criarReservaModal">
