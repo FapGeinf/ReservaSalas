bd684ee237ac08b8b6352e105374cbcb968248db
Joaliny Furtado <Joalinyfurtado87@gmail.com>
2025-01-17 16:53:19 -0400

    comentando a seed sem funcionalidade

--- database/seeders/UnidadesTableSeeder.php
+++ database/seeders/UnidadesTableSeeder.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Seeder;
 
 class UnidadesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    { 
-        Unidade::create(['nome' => 'Setor A', 'sigla' => 'A']); 
-        Unidade::create(['nome' => 'Setor B', 'sigla' => 'B']); 
-        Unidade::create(['nome' => 'Setor C', 'sigla' => 'C']); 
-    }
+    // /**
+    //  * Run the database seeds.
+    //  */
+    // public function run(): void
+    // { 
+    //     Unidade::create(['nome' => 'Setor A', 'sigla' => 'A']); 
+    //     Unidade::create(['nome' => 'Setor B', 'sigla' => 'B']); 
+    //     Unidade::create(['nome' => 'Setor C', 'sigla' => 'C']); 
+    // }
 }
