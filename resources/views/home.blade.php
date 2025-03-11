3eba5513298b873b51cdfd65b063ee55c37d0e44
Leo S. <contato.prof.leonardo@gmail.com>
2025-03-11 09:31:12 -0400

    clareamento do fundo do backdrop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,13 @@
 <link rel="stylesheet" href="{{ asset('css/bg.css') }}">
 <link rel="stylesheet" href="{{ asset('css/input-text.css') }}">
 
+<style>
+  .modal-backdrop {
+  background-color: rgba(0, 0, 0, 0.5) !important; /* Mais claro que o padrão (0.5) */
+}
+
+</style>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
@@ -22,87 +29,25 @@
 
     <div class="row">
     @foreach($salas as $index => $sala)
-    
-
-      <!-- <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
-        <div class="card border position-relative">
-
-          <div class="bg__card_pattern bg__card_pattern_footer text-light text-center mb-0">
-          <img class="fit-image" src="{{ asset('img/salas/' . $sala->imagem) }}" alt="{{ $sala->nome }}">
-            @if(!$sala->situacao)
-              <div class="overlay">
-                <span class="overlay-text">Sala em Manutenção</span>
-              </div>
-            @endif
-          </div>
-
-          <div class="card-body card-fofinho">
-            <div class="title-teste text-center d-flex flex-column" style="margin-bottom: 1rem; margin-top: .5rem;">
-              <span>Local</span>
-              
-               <h3 class="fw-bold text-uppercase word-wrap: normal">{{ $sala->nome }}</h3> -->
-              <!-- <h3 class="fw-bold text-uppercase" style="white-space: nowrap; ">{{ $sala->nome }}</h3>
-
-              <span class="mt-2">Descrição</span>
-              <span class="" style="color:rgb(134, 132, 132); font-size: 14px;">
-                <p class="card-text">{{ $sala->descricao }}</p>
-              </span>
-            </div>
-          </div>
-
-
-          <div class="card-body card-fofinho" style="background-color: #f1f1f1; padding: 10px 30px;">
-            <div class="title-teste text-center d-flex flex-column">
-              <div class="d-flex justify-content-center gap-3 py-2">
-              @php
-               $situacao = strtolower(trim($sala->situacao));
-             @endphp
-
-             <button 
-               type="button" 
-               class="button-green-index {{ $situacao == 'inativa' ? 'disabled' : '' }}" 
-               data-bs-toggle="modal" 
-               data-bs-target="{{ $situacao == 'ativa' ? '#criarReservaModal' : '' }}" 
-               onclick="{{ $situacao == 'ativa' ? 'selecionarSala(' . $sala->id . ')' : 'return false;' }}"
-               {{ $situacao == 'inativa' ? 'disabled' : '' }}>
-               Reservar
-             </button>
-
-                <button 
-                  type="button" 
-                  class="button-blue" 
-                  data-bs-toggle="modal" 
-                  data-bs-target="#verReservasModal" 
-                  onclick="carregarReservas({{ $sala->id }})"
-                  style="font-size: 15px;">
-                  Ver Reservas
-                </button>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div> --> 
 
       <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
         <div class="card border position-relative">
 
-        <!-- Imagem da Sala com Sobreposição -->
-        <div class="bg__card_pattern bg__card_pattern_footer text-light text-center mb-0 position-relative">
-          <img class="fit-image" src="{{ asset('img/salas/' . $sala->imagem) }}" alt="{{ $sala->nome }}">
+          <!-- Imagem da Sala com Sobreposição -->
+          <div class="bg__card_pattern bg__card_pattern_footer text-light text-center mb-0 position-relative">
+            <img class="fit-image" src="{{ asset('img/salas/' . $sala->imagem) }}" alt="{{ $sala->nome }}">
 
-          <!-- Sobreposição para Sala Inativa -->
-          @php
-            $situacao = strtolower(trim($sala->situacao)); // Normaliza o valor de 'situacao'
-          @endphp
+            <!-- Sobreposição para Sala Inativa -->
+            @php
+              $situacao = strtolower(trim($sala->situacao)); // Normaliza o valor de 'situacao'
+            @endphp
 
-          @if($situacao === 'inativa')
-            <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center" 
-                style="background-color: rgba(0, 0, 0, 0.7);">
-              <span class="text-white fw-bold fs-5">Sala em Manutenção</span>
-            </div>
-          @endif
-        </div>
+            @if($situacao === 'inativa')
+              <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center" style="background-color: rgba(0, 0, 0, 0.7);">
+                <span class="text-white fw-bold fs-5">Sala em Manutenção</span>
+              </div>
+            @endif
+          </div>
 
           <!-- Corpo do Card -->
           <div class="card-body card-fofinho">
@@ -322,47 +267,6 @@
     document.getElementById('sala_fk').value = salaId;
 }
 
-
-
-//   function carregarReservas(salaId) {
-//   $('#reservasContainer').html('<p class="text-center"><i class="fa-regular fa-spinner" style="color: #2a64e7;"></i> Carregando reservas...</p>');
-
-//   $.ajax({
-//     url: '/reservas/dia/' + salaId,
-//     type: 'GET',
-//     success: function (reservas) {
-//       let html = '';
-
-//       if (reservas.length === 0) {
-//         html = '<p class="text-center"><i class="fa-solid fa-x me-1" style="color: #b22720;"></i> Nenhuma reserva para hoje.</p>';
-//       } else {
-//         // Criar um container flexível para exibir em formato de grid
-//         html += '<div class="reservas-grid">';
-
-//         reservas.forEach(reserva => {
-//           const unidade = reserva.user?.unidade?.nome ?? 'Unidade Desconhecida';
-//           const usuario = reserva.user ? reserva.user.name : 'N/A';
-//           const horaInicio = reserva.data_inicio.split(' ')[1];
-//           const horaFim = reserva.data_fim.split(' ')[1];
-
-//           // Cada reserva será exibida como um card separado
-//           html += `<div class="reserva-card">
-//                     <h5 class="text">🏢 Unidade: ${unidade}</h5>
-//                     <p><strong>🕒 Hora:</strong> ${horaInicio} - ${horaFim}</p>
-//                     <p><strong>👤 Reservado por:</strong> ${usuario}</p>
-//                   </div>`;
-//         });
-
-//         html += '</div>'; // Fechar o container grid
-//       }
-
-//       $('#reservasContainer').html(html);
-//     },
-//     error: function () {
-//       $('#reservasContainer').html('<p class="text-center"><i class="fa-solid fa-x me-1" style="color: #b22720;"></i> Nenhuma reserva para hoje.</p>');
-//     }
-//   });
-// }
 function carregarReservas(salaId) {
   const dataSelecionada = document.getElementById('dataSelecionada').value;
 
@@ -420,30 +324,6 @@ $(document).ready(function () {
   });
 });
 </script>
-
-<!-- modal de ver reservas -->
-<!-- <div class="modal fade" id="verReservasModal" tabindex="-1" aria-labelledby="verReservasModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-md">
-    <div class="modal-content custom-modal">
-      
-      <div class="modal-header custom-header">
-
-        <h5 class="modal-title w-100 text-center">📅 Reservas do Dia</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-
-      </div>
-
-      <div class="modal-body">
-        <div id="reservasContainer" class="reservas-container">
-          <p class="text-center text-muted">
-            <i class="fa-regular fa-spinner" style="color: #2a64e7;"></i> Carregando reservas...
-          </p>
-        </div>
-      </div>
-
-    </div>
-  </div>
-</div> -->
 
 <div class="modal fade" id="verReservasModal" tabindex="-1" aria-labelledby="verReservasModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-md">
