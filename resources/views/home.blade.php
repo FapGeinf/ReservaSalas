0b7a1201ef922b9c228f3cf31ad95b66bcca5aad
Joaliny Furtado <Joalinyfurtado87@gmail.com>
2025-04-24 17:01:43 -0400

    atualizando o fullcalendar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,8 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+
+
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
@@ -328,6 +330,9 @@ function toggleDropdown(button) {
 <!-- Adicione a biblioteca SweetAlert2 no <head> -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
+
+
 
 
 <script>
@@ -335,13 +340,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth', // Visualização inicial
+        initialView: 'timeGridWeek',
         locale: 'pt-br', // Idioma
         events: '/eventos', // URL para buscar os eventos
         selectable: true,
         editable: false,
         eventDisplay: 'block',
 
+
+        // Campo que mostrar as reservas no calendar do evento
         eventContent: function(arg) {
             const horaInicio = arg.event.extendedProps.hora_inicio || '';
             const horaFim = arg.event.extendedProps.hora_fim || '';
@@ -349,7 +356,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const nomeSala = arg.event.title || '';
 
             let innerHtml = `
-        <div style="font-size: 0.85em;">
+        <div style="font-size: 0.95em;">
             <strong>${nomeSala}</strong><br>
             ${horaInicio} - ${horaFim}<br>
             ${responsavel}
