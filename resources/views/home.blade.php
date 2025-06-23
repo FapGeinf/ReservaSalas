3b77c42cc6997c9cae1c31e549c3c66694088190
Joaliny Furtado <Joalinyfurtado87@gmail.com>
2025-06-23 16:07:32 -0400

    adicionando funcionalidade para  o botão de reservar o dia inteiro

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -265,6 +265,30 @@
     </div>
 </div>
 
+
+<script>
+document.getElementById('diaInteiro').addEventListener('change', function () {
+    const inicio = document.getElementById('hora_inicio');
+    const termino = document.getElementById('hora_termino');
+    if (this.checked) {
+        // marca com o dia inteiro: define horários e desativa Edição
+
+        inicio.value = '08:00';
+        termino.value = '20:00';
+        inicio.readOnly = true;
+        termino.readOnly = true;
+    } else {
+        // permite editar novamente
+        inicio.readOnly = false;
+        termino.readOnly = false;
+        inicio.value = '';
+        termino.value = "";
+    }
+    }
+);
+
+</script>
+
 <script>
     document.getElementById('dataSelecionada').addEventListener('change', function() {
         const dataSelecionada = new Date(this.value);
