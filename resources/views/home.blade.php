1ca2b62348f3a5afb0409ee0a4b674dfabeae6fb
Leo S. <contato.prof.leonardo@gmail.com>
2025-07-09 14:15:36 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -478,7 +478,7 @@ document.getElementById('diaInteiro').addEventListener('change', function () {
 
                 return {
                     html: `
-                    <div style="font-size: 0.95em; color: #555555;">
+                    <div style="font-size: .9rem;">
                         <span class="fw-bold text-uppercase">${nomeSala}</span><br>
                         <i class="bi bi-clock" style="font-size: 11px; position: relative; top: -1px;"></i> 
                         ${horaInicio} - ${horaFim}<br>
