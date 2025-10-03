337b3ee7906e0ec028763d6df0ce90592ae7b10b
Leo S. <contato.prof.leonardo@gmail.com>
2025-10-03 16:07:04 -0400

    Criado arquivo para a função

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,9 @@
 
 {{-- Não sei se esse botão é necessário --}}
 <script src="{{ asset('js/toggleDropdown.js') }}"></script>
+
 <script src="{{ asset('js/miniCalendar.js') }}"></script>
+<script src="{{ asset('js/abrirModalEdicao.js') }}"></script>
 <script src="{{ asset('js/modal/abrirModalDetalhes.js') }}"></script>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
@@ -300,44 +302,6 @@
     </div>
   </div>
 </div>
-
-
-
-
-<script>
-  function abrirModalEdicao(id, horaInicio, horaFim, dataInicio, salaId) {
-    if (!id) {
-      alert('Erro: ID da reserva ausente!');
-      return;
-    }
-
-    // Preenche os campos
-    document.getElementById('data_inicio').value = dataInicio;
-    document.getElementById('hora_inicio').value = horaInicio;
-    document.getElementById('data_fim').value = horaFim;
-    document.getElementById('reserva-numero').textContent = `Reserva ${id}`;
-    document.getElementById('form-editar-reserva').action = `/reservas/${id}`;
-
-    // Preenche o select de salas
-    const selectSala = document.getElementById('sala_id');
-    selectSala.innerHTML = ''; // limpa opções anteriores
-
-    salasDisponiveis.forEach(sala => {
-      const option = document.createElement('option');
-      option.value = sala.id;
-      option.textContent = sala.nome;
-
-      if (sala.id === salaId) {
-        option.selected = true;
-      }
-      selectSala.appendChild(option);
-    });
-
-    // Abre o modal
-    const modal = new bootstrap.Modal(document.getElementById('modal-editar-reserva'));
-    modal.show();
-  }
-</script>
 
 <script>
   // Função para abrir o modal do calendário e selecionar uma sala
