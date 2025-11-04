9510d7798916b12824c823dff64013fb11df895a
Leo S. <contato.prof.leonardo@gmail.com>
2025-11-04 15:36:12 -0400

    Padronização da página

--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title') {{ 'Cadastrar Novo Usuário' }} @endsection
+@section('title') {{ 'Novo Usuário' }} @endsection
 @section('content')
 
 <link rel="stylesheet" href="{{ asset('css/salas.css') }}">
@@ -8,28 +8,6 @@
 <link rel="stylesheet" href="{{ asset('css/buttons.css') }}">
 <link rel="stylesheet" href="{{ asset('css/form-custom.css') }}">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
-
-<style>
-  /* Removendo margem apenas para campos que não sejam senha */
-  .form-line-split > input:first-child {
-    margin-left: 42px;
-  }
-
-  .split-password {
-    margin-left: 54px;
-  }
-
-  /* Mobile */
-  @media (max-width: 768px) {
-    .form-line-split > input:first-child {
-      margin-left: 0px;
-    }
-
-    .split-password {
-      margin-left: 0px;
-    }
-  }
-</style>
 
 <script>
   function togglePassword(inputId, iconId) {
@@ -64,52 +42,54 @@
   @endif
 @endpush
 
-<div class="form-custom no-border-bottom form-no-bottom mt-5">
+<div class="form-custom no-border-bottom form-no-bottom mt-5" style="max-width: 480px;">
   <h5 class="fw-bold text-center text-uppercase mb-3">
     Novo Usuário
   </h5>
 
   <form id="new-user" method="POST" action="{{ route('usuarios.store') }}">
     @csrf
-    
-    <div class="form-line mt-4">
-      <label for="name" class="fw-bold">Nome:</label>
-      <input type="text" class="input-custom" id="name" name="name" required>
+
+    <div class="row g-3 mt-3">
+      <div class="col-12 col-sm-7">
+        <label for="name" class="fw-bold">Nome:</label>
+        <input type="text" class="input-custom" id="name" name="name" required>        
+      </div>
+
+      <div class="col-12 col-sm-5">
+        <label class="fw-bold">Login:</label>
+        <input type="text" class="input-custom" id="login" name="login" required>        
+      </div>
+
+      <div class="col-12 col-sm-7">
+        <label class="fw-bold">Tipo de Usuário:</label>
+        <select class="form-select pointer" id="role" name="role" required>
+          <option value="user">Usuário Comum</option>
+          <option value="admin">Administrador</option>
+        </select>
+      </div>
+
+      <div class="col-12">
+        <label for="email" class="fw-bold">Email:</label>
+        <input type="email" class="input-custom" id="email" name="email" required>        
+      </div>
+
+      <div class="col-12">
+        <label for="unidade_fk" class="fw-bold">Unidade:</label>
+        <select class="form-select pointer" id="unidade_fk" name="unidade_fk" required>
+          <option value="">Selecione a unidade</option>
+
+          @foreach($unidades as $unidade)
+            <option value="{{ $unidade->id }}">{{ $unidade->nome }}</option>
+          @endforeach
+        </select>        
+      </div>
     </div>
 
-    <div class="form-line mt-4">
-      <label for="email" class="fw-bold">Email:</label>
-      <input type="email" class="input-custom" id="email" name="email" required>
-    </div>
+    <div class="row g-3 mt-1">
+      <div class="col-12 col-sm-6">
+        <label class="fw-bold">Crie uma senha:</label>
 
-    <div class="form-line mt-4">
-      <label for="unidade_fk" class="fw-bold">Unidade:</label>
-      <select class="form-select pointer" id="unidade_fk" name="unidade_fk" required>
-        <option value="">Selecione a unidade</option>
-
-        @foreach($unidades as $unidade)
-          <option value="{{ $unidade->id }}">{{ $unidade->nome }}</option>
-        @endforeach
-      </select>
-    </div>
-
-    <div class="form-line mt-4">
-      <label class="fw-bold">Login:</label>
-      <input type="text" class="input-custom" id="login" name="login" required>
-    </div>
-
-    <div class="form-line mt-4">
-      <label class="fw-bold">Tipo de Usuário:</label>
-      <select class="form-select pointer" id="role" name="role" required>
-        <option value="user">Usuário Comum</option>
-        <option value="admin">Administrador</option>
-      </select>
-    </div>
-
-    <div class="form-line mt-4">
-      <label class="fw-bold">Senha/ Repita a senha:</label>
-
-      <div class="form-line-split split-password">
         <div style="position: relative;">
           <input type="password" class="input-custom" id="password" name="password" required>
           <i id="eyePassword" class="bi bi-eye" 
@@ -123,6 +103,10 @@
             onclick="togglePassword('password', 'eyePassword')">
           </i>
         </div>
+      </div>
+
+      <div class="col-12 col-sm-6">
+        <label class="fw-bold">Repita a senha:</label>
 
         <div style="position: relative;">
           <input type="password" class="input-custom" id="password_confirmation" name="password_confirmation" required>
@@ -135,14 +119,13 @@
             cursor: pointer;"
             onclick="togglePassword('password_confirmation', 'eyePasswordConfirmation')">
           </i>
-        </div>
+        </div>        
       </div>
     </div>
-
   </form>
 </div>
 
-<div class="form-custom no-border-top form-no-top pb-3" style="padding-top: 13px;">
+<div class="form-custom no-border-top form-no-top pb-3" style="padding-top: 12px; max-width: 480px;">
   <div class="d-flex justify-content-end">
     <button type="submit" form="new-user" class="button-green">
       <i class="bi bi-save me-1"></i>
