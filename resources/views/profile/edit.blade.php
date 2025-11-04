db7e543519427d58ec9afd02a1771af6ac69ae75
Leo S. <contato.prof.leonardo@gmail.com>
2025-11-04 15:07:06 -0400

    Padronização da página

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,19 +9,9 @@
 <link rel="stylesheet" href="{{ asset('css/form-custom.css') }}">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
 
-<style>
-  .form-line-split input:first-child {
-    margin-left: 42px;
-  }
+<x-alert-toast/>
 
-  @media (max-width: 768px) {
-    .form-line-split input:first-child {
-      margin-left: 0px;
-    }
-  }
-</style>
-
-<div class="form-custom no-border-bottom form-no-bottom mt-5">
+<div class="form-custom no-border-bottom form-no-bottom mt-5" style="max-width: 480px;">
   <h5 class="fw-bold text-center text-uppercase">
     Editar Perfil
   </h5>
@@ -30,53 +20,51 @@
     @csrf
     @method('PATCH')
 
-    @if (session('status'))
-      <div class="alert alert-success text-center">
-        {{ session('status') }}
+    <div class="row g-3 mt-3">
+      <div class="col-12">
+        <label for="name" class="fw-bold">Nome Completo:</label>
+        <input type="text" id="name" name="name" class="input-custom" value="{{ old('name', auth()->user()->name) }}" required placeholder="ex: Julliany Souza" autocomplete="name">
+
+        <x-input-error :messages="$errors->get('name')" class="mt-2" />
       </div>
-    @endif
 
-    <div class="form-line mt-4">
-      <label for="name" class="fw-bold">Nome Completo:</label>
-      <input type="text" id="name" name="name" class="input-custom" value="{{ old('name', auth()->user()->name) }}" required placeholder="ex: Julliany Souza" autocomplete="name">
+      <div class="col-12">
+        <label for="email" class="fw-bold">Email:</label>
+        <input type="email" id="email" name="email" class="input-custom" value="{{ old('email', auth()->user()->email) }}"
+        required placeholder="ex: meuemail@email.com" autocomplete="username">
 
-      <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+      </div>
+
+      <div class="col-12">
+        <label for="unidade_fk" class="fw-bold">Unidade:</label>
+
+        <select name="unidade_fk" id="unidade_fk" class="form-control form-select input-custom pointer" required>
+          <option value="" selected disabled>Selecione a unidade</option>
+          @foreach($unidades as $unidade)
+            <option value="{{ $unidade->id }}" {{ auth()->user()->unidade_fk == $unidade->id ? 'selected' : '' }}>{{ $unidade->nome }}</option>
+          @endforeach
+        </select>
+      </div>
     </div>
 
-    <div class="form-line mt-4">
-      <label for="email" class="fw-bold">Email:</label>
-      <input type="email" id="email" name="email" class="input-custom" value="{{ old('email', auth()->user()->email) }}"
-      required placeholder="ex: meuemail@email.com" autocomplete="username">
-
-      <x-input-error :messages="$errors->get('email')" class="mt-2" />
-    </div>
-
-    <div class="form-line mt-4">
-      <label for="unidade_fk" class="fw-bold">Unidade:</label>
-
-      <select name="unidade_fk" id="unidade_fk" class="form-control form-select input-custom pointer" required>
-        <option value="" selected disabled>Selecione a unidade</option>
-        @foreach($unidades as $unidade)
-          <option value="{{ $unidade->id }}" {{ auth()->user()->unidade_fk == $unidade->id ? 'selected' : '' }}>{{ $unidade->nome }}</option>
-        @endforeach
-      </select>
-    </div>
-
-    <div class="form-line col-10 mt-4">
-      <label for="password" class="fw-bold">Senha:</label>
+    <div class="row g-3 mt-1">
+      <div class="col-12 col-sm-6">
+        <label for="password" class="fw-bold">Senha:</label>
         <input type="password" id="password" name="password" class="input-custom" placeholder="Mínimo de 8 caracteres" autocomplete="new-password">
         <x-input-error :messages="$errors->get('password')" class="mt-2" />
-    </div>
+      </div>
 
-    <div class="form-line col-10 mt-4">
-      <label for="password_confirmation" class="fw-bold">Repita a senha:</label>
-      <input type="password" id="password_confirmation" name="password_confirmation" class="input-custom" placeholder="Repita a senha" autocomplete="new-password">
+      <div class="col-12 col-sm-6">
+        <label for="password_confirmation" class="fw-bold">Repita a senha:</label>
+        <input type="password" id="password_confirmation" name="password_confirmation" class="input-custom" placeholder="Repita a senha" autocomplete="new-password">
         <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+      </div>
     </div>
   </form>
 </div>
 
-<div class="form-custom no-border-top form-no-top pt-3">
+<div class="form-custom no-border-top form-no-top pt-3" style="max-width: 480px;">
   <div class="d-flex justify-content-end pb-3">
     <button type="submit" form="form-profile" class="button-green">
       <i class="bi bi-save me-1"></i>
@@ -87,5 +75,4 @@
 
 @endsection
 
-<!-- bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
