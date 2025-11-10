db2124bf4b9a3e95e1c49aa6679cb574a0f525d4
Leo S. <contato.prof.leonardo@gmail.com>
2025-11-10 11:17:04 -0400

    Remoção de comentário desnecessário

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,37 +84,6 @@
           @endforeach
         </div>
       </div>
-
-      {{-- <div class="ver-reservas-container border rounded shadow-sm d-flex flex-column"
-        style="background-color: #fff; padding: 1rem;" data-help="pesquisa-reservas">
-
-        <h6 class="fw-bold text-center mb-3">Consulta de Reservas</h6>
-
-        <span class="text-muted mb-4 text-center" style="font-size: 13px;">
-          Escolha a <span class="fw-semibold" style="color: #374151;">sala</span> e a <span class="fw-semibold" style="color: #374151;">data</span> desejadas para visualizar as reservas já feitas.
-        </span>
-
-        <div class="row g-3 mb-3">
-          <div class="col-12 col-sm-7">
-            <label for="salaSelecionada" class="fw-semibold">Sala:</label>
-            <select id="salaSelecionada" class="input-custom form-select pointer w-100">
-              <option value="">Selecione uma sala</option>
-            </select>
-          </div>
-
-          <div class="col-12 col-sm-5">
-            <label for="dataSelecionada" class="fw-semibold">Data:</label>
-            <input type="date" id="dataSelecionada" class="input-custom w-100">
-          </div>
-        </div>
-
-        <div id="reservasContainer" class="reservas-container flex-grow-1 overflow-auto"
-          style="max-height: 450px;">
-          <p class="text-center text-muted">
-            <i class="bi bi-arrow-repeat" style="color: #2a64e7;"></i> Carregando reservas...
-          </p>
-        </div>
-      </div> --}}
     </div>
 
     <div class="col-12 col-lg-8 px-lg-3">
@@ -285,7 +254,7 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h6 class="modal-title">Detalhes da Reserva</h6>
+        <h6 class="modal-title">Detalhes da Reserva1</h6>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
       </div>
 
