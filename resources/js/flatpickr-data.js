77a2251dd244d74ef02e207527eb1a7fd3cc93b3
Leo S. <contato.prof.leonardo@gmail.com>
2025-11-14 15:25:39 -0400

    Update flatpickr-data.js

--- resources/js/flatpickr-data.js
+++ resources/js/flatpickr-data.js
@@ -2,8 +2,8 @@ import flatpickr from "flatpickr";
 import { Portuguese } from "flatpickr/dist/l10n/pt.js";
 
 flatpickr("#dataSelecionada", {
-    locale: Portuguese,
-    altInput: true,
-    altFormat: "d/m/Y",
-    dateFormat: "Y-m-d"
+  locale: Portuguese,
+  altInput: true,
+  altFormat: "d/m/Y",
+  dateFormat: "Y-m-d"
 });
