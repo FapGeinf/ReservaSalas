a7bfdfa0c5132bb056ac0556a02444b3697f0f1c
Leo S. <contato.prof.leonardo@gmail.com>
2026-02-05 16:25:20 -0400

    Correção na navbar

--- resources/views/reservas/reservas.blade.php
+++ resources/views/reservas/reservas.blade.php
@@ -108,30 +108,30 @@
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
 <script>
   // Definir o plugin de ordenação personalizada ANTES de usar
-  jQuery.extend(jQuery.fn.dataTableExt.oSort, {
-    "date-euro-pre": function(a) {
-      if ($.trim(a) !== '') {
-        var parts = a.split(' | ');
-        var dateParts = parts[0].split('/');
-        var timeParts = parts[1].split(':');
+  // jQuery.extend(jQuery.fn.dataTableExt.oSort, {
+  //   "date-euro-pre": function(a) {
+  //     if ($.trim(a) !== '') {
+  //       var parts = a.split(' | ');
+  //       var dateParts = parts[0].split('/');
+  //       var timeParts = parts[1].split(':');
 
-        return new Date(
-          dateParts[2], // ano
-          dateParts[1] - 1, // mês (0-11)
-          dateParts[0], // dia
-          timeParts[0], // horas
-          timeParts[1] // minutos
-        ).getTime();
-      }
-      return 0;
-    },
-    "date-euro-asc": function(a, b) {
-      return a - b;
-    },
-    "date-euro-desc": function(a, b) {
-      return b - a;
-    }
-  });
+  //       return new Date(
+  //         dateParts[2], // ano
+  //         dateParts[1] - 1, // mês (0-11)
+  //         dateParts[0], // dia
+  //         timeParts[0], // horas
+  //         timeParts[1] // minutos
+  //       ).getTime();
+  //     }
+  //     return 0;
+  //   },
+  //   "date-euro-asc": function(a, b) {
+  //     return a - b;
+  //   },
+  //   "date-euro-desc": function(a, b) {
+  //     return b - a;
+  //   }
+  // });
 
   $(document).ready(function() {
     // Inicialização única da DataTable
@@ -167,82 +167,82 @@
     });
 
     // Função para o modal de confirmação de exclusão
-    function setDeleteAction(action) {
-      $('#deleteForm').attr('action', action);
-    }
+    // function setDeleteAction(action) {
+    //   $('#deleteForm').attr('action', action);
+    // }
 
     // Exemplo de como você poderia usar (adaptar conforme necessário)
-    $('.btn-delete').on('click', function() {
-      var deleteUrl = $(this).data('url');
-      setDeleteAction(deleteUrl);
-    });
+    // $('.btn-delete').on('click', function() {
+    //   var deleteUrl = $(this).data('url');
+    //   setDeleteAction(deleteUrl);
+    // });
   });
 
-  function selecionarSala(salaId) {
-    console.log('Sala selecionada:', salaId); // Depuração
-    document.getElementById('sala_fk').value = salaId;
-  }
+  // function selecionarSala(salaId) {
+  //   console.log('Sala selecionada:', salaId); // Depuração
+  //   document.getElementById('sala_fk').value = salaId;
+  // }
 
-  function carregarReservas(salaId) {
-    const dataSelecionada = document.getElementById('dataSelecionada').value;
+  // function carregarReservas(salaId) {
+  //   const dataSelecionada = document.getElementById('dataSelecionada').value;
 
-    $('#reservasContainer').html(
-      '<p class="text-center"><i class="bi bi-arrow-repeat" style="color: #2a64e7;"></i> Carregando reservas...</p>'
-    );
+  //   $('#reservasContainer').html(
+  //     '<p class="text-center"><i class="bi bi-arrow-repeat" style="color: #2a64e7;"></i> Carregando reservas...</p>'
+  //   );
 
-    $.ajax({
-      url: '/reservas/sala/' + salaId, // Rota para buscar as reservas da sala
-      type: 'GET',
-      data: {
-        data: dataSelecionada
-      },
+  //   $.ajax({
+  //     url: '/reservas/sala/' + salaId, // Rota para buscar as reservas da sala
+  //     type: 'GET',
+  //     data: {
+  //       data: dataSelecionada
+  //     },
 
-      success: function(reservas) {
-        let html = '';
+  //     success: function(reservas) {
+  //       let html = '';
 
-        if (reservas.length === 0) {
-          html = '<p class="reserva-vazia">Nenhuma reserva para esta data.</p>';
-        } else {
+  //       if (reservas.length === 0) {
+  //         html = '<p class="reserva-vazia">Nenhuma reserva para esta data.</p>';
+  //       } else {
 
-          html += '<div class="reservas-grid">';
-          reservas.forEach(reserva => {
-            const unidade = reserva.user?.unidade?.nome ?? 'Unidade Desconhecida';
-            const usuario = reserva.user ? reserva.user.name : 'N/A';
-            const horaInicio = reserva.data_inicio.split(' ')[1];
-            const horaFim = reserva.data_fim.split(' ')[1];
+  //         html += '<div class="reservas-grid">';
+  //         reservas.forEach(reserva => {
+  //           const unidade = reserva.user?.unidade?.nome ?? 'Unidade Desconhecida';
+  //           const usuario = reserva.user ? reserva.user.name : 'N/A';
+  //           const horaInicio = reserva.data_inicio.split(' ')[1];
+  //           const horaFim = reserva.data_fim.split(' ')[1];
 
-            html += `
-              <div class="reserva-card">
-                <span class="reserva-info">
-                  <i class="bi bi-building"></i>
-                  <strong>Unidade:</strong> ${unidade}
-                </span>
+  //           html += `
+  //             <div class="reserva-card">
+  //               <span class="reserva-info">
+  //                 <i class="bi bi-building"></i>
+  //                 <strong>Unidade:</strong> ${unidade}
+  //               </span>
 
-                <span class="reserva-info">
-                    <i class="bi bi-clock"></i>
-                    <strong>Hora:</strong> ${horaInicio} - ${horaFim}
-                </span>
+  //               <span class="reserva-info">
+  //                   <i class="bi bi-clock"></i>
+  //                   <strong>Hora:</strong> ${horaInicio} - ${horaFim}
+  //               </span>
 
-                <span class="reserva-info">
-                  <i class="bi bi-person"></i>
-                  <strong>Reservado por:</strong> ${usuario}
-                </span>
-              </div>
-              `;
-            });
-          html += '</div>';
-        }
+  //               <span class="reserva-info">
+  //                 <i class="bi bi-person"></i>
+  //                 <strong>Reservado por:</strong> ${usuario}
+  //               </span>
+  //             </div>
+  //             `;
+  //           });
+  //         html += '</div>';
+  //       }
 
-        $('#reservasContainer').html(html);
-      },
+  //       $('#reservasContainer').html(html);
+  //     },
 
-      error: function() {
-        $('#reservasContainer').html(
-          '<p class="text-center"><i class="bi bi-exclamation-circle-fill me-1" style="color: #b22720;"></i> Erro ao carregar reservas.</p>'
-        );
-      }
-    });
-  }
+  //     error: function() {
+  //       $('#reservasContainer').html(
+  //         '<p class="text-center"><i class="bi bi-exclamation-circle-fill me-1" style="color: #b22720;"></i> Erro ao carregar reservas.</p>'
+  //       );
+  //     }
+  //   });
+  // }
 
   $(document).ready(function() {
     $('#dataSelecionada').on('change', function() {
@@ -333,3 +333,5 @@
   });
   </script>
 @endif
+
+@endsection
