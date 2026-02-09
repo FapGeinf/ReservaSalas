4091e288975a580e99803847aaf217b9662c0686
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2026-02-09 09:24:07 -0400

    Correção commit passado, buscar unidade ususario no ad

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -26,72 +26,8 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
-    {
-        $request->validate([
-            'login'    => ['required', 'string'],
-            'password' => ['required', 'string'],
-        ]);
-        
-    
-        $domain = env('LDAP_AUTH_DOMAIN');
 
-        $loginInput = $request->login;
-        $passwordInput = $request->password;
-
-        $loginSam = trim($loginInput );
-        $ldapLogin =  $loginSam . '@' . $domain;
-
-        try {
-            if (!Container::getConnection('default')
-                ->auth()
-                ->attempt($ldapLogin, $passwordInput)
-            ) {
-                return back()->withErrors([
-                    'login' => 'Usuário ou senha inválidos no AD.',
-                ]);
-            }
-
-            
-            $ldapUser = LdapUser::where(
-                'samaccountname',
-                $loginInput
-            )->first();
-            
-
-            if (!$ldapUser) {
-                return back()->withErrors([
-                    'login' => 'Usuário autenticou, mas não foi encontrado no AD.',
-                ]);
-            }
-
-            $domain = env('LDAP_AUTH_DOMAIN');
-            $user = User::updateOrCreate(
-                ['guid' => $ldapUser->getConvertedGuid()],
-                [
-                    'login'         => $loginSam,
-                    'username'      => $loginSam,
-                    'name'          => $ldapUser->getFirstAttribute('displayname') ?? $loginSam,
-                    'email'         => $ldapUser->getFirstAttribute('mail'),
-                    'domain'        => $domain,
-                    'auth_provider' => 'ldap',
-                    'password'      => null,
-                ]
-            );
-
-            
-            Auth::login($user);
-            $request->session()->regenerate();
-
-            return redirect()->intended('dashboard');
-
-        } catch (\Throwable $e) {
-            return back()->withErrors([
-                'login' => 'Erro ao autenticar no Active Directory.',
-            ]);
-        }
-    }public function store(LoginRequest $request): RedirectResponse
-{
+    public function store(LoginRequest $request): RedirectResponse{
     $request->validate([
         'login'    => ['required', 'string'],
         'password' => ['required', 'string'],
