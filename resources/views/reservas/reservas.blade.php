ed771a6557c2803305f55caef25a1a83539779ad
Leo S. <contato.prof.leonardo@gmail.com>
2026-02-10 12:02:56 -0400

    Padronização e exclusão de código desnecessário

--- resources/views/reservas/reservas.blade.php
+++ resources/views/reservas/reservas.blade.php
@@ -14,100 +14,118 @@
 <link rel="stylesheet" href="{{ asset('css/swal-alert.css') }}">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <script src="js/custom.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/locales-all.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<style>
+  #reservas {
+    border-collapse: separate;
+    border-spacing: 0;
+  }
+
+  /* TH */
+  #reservas thead th {
+    border-right: 1px solid #ccc;
+  }
+  #reservas thead th:last-child {
+    border-right: none;
+  }
+
+  /* TD */
+  #reservas tbody td {
+    border-right: 1px solid #ccc;
+  }
+  #reservas tbody td:last-child {
+    border-right: none;
+  }
+</style>
 
 <div class="container mt-5">
   <div class="tabela-main-page">
     <div class="text-center fw-bold mb-4">
-      <span class="title-meetings text-uppercase">Lista de Reuniões</span>
+      <span class="title-meetings">Lista de Reuniões</span>
     </div>
 
-    <table class="table-reservas table-striped" id="reservas">
-      <thead>
-        <tr>
-          <th><label class="text-light">Id</label></th>
-          <th><label class="text-light">Sala</label></th>
-          <th><label class="text-light">Hora Início</label></th>
-          <th><label class="text-light">Hora Término</label></th>
-          <th><label class="text-light">Reservado Por</label></th>
-          <th><label class="text-light">Unidade</label></th>
-          <th data-label="Tipo Reserva"><label class="text-light">Tipo de Reserva</label></th>
-          <th><label class="text-light">Opções</label></th>
-        </tr>
-      </thead>
-
-      <tbody>
-        @foreach($reservas as $reserva)
+    <div class="table-responsive">
+      <table id="reservas" class="table table-striped border-bottom-0 my-3">
+        <thead>
           <tr>
-            <td data-label="Id" class="fs-13">{{ $reserva->id }}</td>
-            <td data-label="Sala" class="fs-13">
-              <div class="mt-1">
-                <p class="mb-1 text-uppercase">
-                  {{ $reserva->sala ? $reserva->sala->nome : 'Sala não encontrada' }}
-                </p>
-              </div>
-            </td>
-
-            <td data-label="Hora Início" class="fs-13">{{ \Carbon\Carbon::parse($reserva->data_inicio)->format('d/m/Y \à\s H:i') }}</td>
-            <td data-label="Hora Término" class="fs-13">{{ \Carbon\Carbon::parse($reserva->data_fim)->format('d/m/Y \à\s H:i') }}</td>
-            <td data-label="Reservado Por" class="fs-13">{{ $reserva->user ? $reserva->user->name : '' }}</td>
-            <td data-label="Unidade" class="fs-13">{{ $reserva->user && $reserva->user->unidade ? $reserva->user->unidade->nome : '' }}</td>
-            <!-- <td>{{ ucfirst($reserva->finalidade) ?? 'N/A' }}</td> -->
-            <td data-label="Tipo Reserva" class="fs-13">{{ ucfirst($reserva->finalidade) ?? '—' }}</td>
-            <td data-label="Opções" class="fs-13">
-              <div class="dropdown">
-                <button class="custom-actions-btn" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="bi bi-three-dots-vertical"></i>
-                </button>
-
-                <ul class="dropdown-menu">
-                  <li>
-                    <a href="{{ route('reservas.show', $reserva->id) }}"
-                      class="dropdown-item text-decoration-none text-pattern fs-13">
-                      <i class="bi bi-info-circle me-1"></i>
-                      Detalhes
-                    </a>
-                  </li>
-
-                  <li>
-                    <a href="{{ route('reservas.edit', $reserva->id) }}"
-                      class="dropdown-item text-decoration-none text-pattern fs-13">
-                      <i class="bi bi-pencil-square me-1"></i>
-                      Editar
-                    </a>
-                  </li>
-
-                  <li>
-                    <button class="dropdown-item text-danger fs-13" data-bs-toggle="modal"
-                      data-bs-target="#confirmDeleteModal" onclick="setDeleteId({{ $reserva->id }})">
-                      <i class="bi bi-trash me-1"></i>
-                      Excluir
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </td>
+            <th>Id</th>
+            <th>Sala</th>
+            <th>Hora Início</th>
+            <th>Hora Término</th>
+            <th>Reservado Por</th>
+            <th>Unidade</th>
+            <th>Tipo de Reserva</th>
+            <th>Opções</th>
           </tr>
-        @endforeach
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          @foreach($reservas as $reserva)
+            <tr>
+              <td data-th="Id" class="fs-13">{{ $reserva->id }}</td>
+              <td data-th="Sala" class="fs-13">
+                <div class="mt-1">
+                  <p class="mb-1 text-uppercase">
+                    {{ $reserva->sala ? $reserva->sala->nome : 'Sala não encontrada' }}
+                  </p>
+                </div>
+              </td>
+
+              <td data-th="Hora Início" class="fs-13">{{ \Carbon\Carbon::parse($reserva->data_inicio)->format('d/m/Y \à\s H:i') }}</td>
+              <td data-th="Hora Término" class="fs-13">{{ \Carbon\Carbon::parse($reserva->data_fim)->format('d/m/Y \à\s H:i') }}</td>
+              <td data-th="Reservado Por" class="fs-13">{{ $reserva->user ? $reserva->user->name : '' }}</td>
+              <td data-th="Unidade" class="fs-13">{{ $reserva->user && $reserva->user->unidade ? $reserva->user->unidade->nome : '' }}</td>
+              <!-- <td>{{ ucfirst($reserva->finalidade) ?? 'N/A' }}</td> -->
+              <td data-th="Tipo Reserva" class="fs-13">{{ ucfirst($reserva->finalidade) ?? '—' }}</td>
+              <td data-th="Opções" class="fs-13">
+                <div class="dropdown">
+                  <button class="custom-actions-btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-three-dots-vertical"></i>
+                  </button>
+
+                  <ul class="dropdown-menu dropdown-menu-dark">
+                    <li>
+                      <a href="{{ route('reservas.show', $reserva->id) }}"
+                        class="dropdown-item text-decoration-none fs-13">
+                        <i class="bi bi-info-circle me-1"></i>
+                        Detalhes
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="{{ route('reservas.edit', $reserva->id) }}"
+                        class="dropdown-item text-decoration-none fs-13">
+                        <i class="bi bi-pencil-square me-1"></i>
+                        Editar
+                      </a>
+                    </li>
+
+                    <li>
+                      <button class="dropdown-item text-danger fs-13" data-bs-toggle="modal"
+                        data-bs-target="#confirmDeleteModal" onclick="setDeleteId({{ $reserva->id }})">
+                        <i class="bi bi-trash me-1"></i>
+                        Excluir
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>      
+    </div>
   </div>
 </div>
 
-<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+
 <script>
-  // Definir o plugin de ordenação personalizada ANTES de usar
   jQuery.extend(jQuery.fn.dataTableExt.oSort, {
     "date-euro-pre": function(a) {
       if ($.trim(a) !== '') {
@@ -178,72 +196,6 @@
     });
   });
 
-  function selecionarSala(salaId) {
-    console.log('Sala selecionada:', salaId); // Depuração
-    document.getElementById('sala_fk').value = salaId;
-  }
-
-  function carregarReservas(salaId) {
-    const dataSelecionada = document.getElementById('dataSelecionada').value;
-
-    $('#reservasContainer').html(
-      '<p class="text-center"><i class="bi bi-arrow-repeat" style="color: #2a64e7;"></i> Carregando reservas...</p>'
-    );
-
-    $.ajax({
-      url: '/reservas/sala/' + salaId, // Rota para buscar as reservas da sala
-      type: 'GET',
-      data: {
-        data: dataSelecionada
-      },
-
-      success: function(reservas) {
-        let html = '';
-
-        if (reservas.length === 0) {
-          html = '<p class="reserva-vazia">Nenhuma reserva para esta data.</p>';
-        } else {
-
-          html += '<div class="reservas-grid">';
-          reservas.forEach(reserva => {
-            const unidade = reserva.user?.unidade?.nome ?? 'Unidade Desconhecida';
-            const usuario = reserva.user ? reserva.user.name : 'N/A';
-            const horaInicio = reserva.data_inicio.split(' ')[1];
-            const horaFim = reserva.data_fim.split(' ')[1];
-
-            html += `
-              <div class="reserva-card">
-                <span class="reserva-info">
-                  <i class="bi bi-building"></i>
-                  <strong>Unidade:</strong> ${unidade}
-                </span>
-
-                <span class="reserva-info">
-                    <i class="bi bi-clock"></i>
-                    <strong>Hora:</strong> ${horaInicio} - ${horaFim}
-                </span>
-
-                <span class="reserva-info">
-                  <i class="bi bi-person"></i>
-                  <strong>Reservado por:</strong> ${usuario}
-                </span>
-              </div>
-              `;
-            });
-          html += '</div>';
-        }
-
-        $('#reservasContainer').html(html);
-      },
-
-      error: function() {
-        $('#reservasContainer').html(
-          '<p class="text-center"><i class="bi bi-exclamation-circle-fill me-1" style="color: #b22720;"></i> Erro ao carregar reservas.</p>'
-        );
-      }
-    });
-  }
-
   $(document).ready(function() {
     $('#dataSelecionada').on('change', function() {
       const salaId = $('#verReservasModal').data('sala-id');
@@ -306,6 +258,7 @@
   <script>
     document.addEventListener('DOMContentLoaded', function() {
       Swal.fire({
+        position: 'top',
         title: 'Sucesso!',
         text: "{{ session('success') }}",
         icon: 'success',
@@ -320,16 +273,17 @@
 
 @if (session('error'))
   <script>
-  document.addEventListener('DOMContentLoaded', function() {
-    Swal.fire({
-      title: 'Ops!',
-      text: "{{ session('error') }}",
-      icon: 'error',
-      confirmButtonText: 'Fechar',
-      customClass: {
-        confirmButton: 'button-red'
-      }
+    document.addEventListener('DOMContentLoaded', function() {
+      Swal.fire({
+        position: 'top',
+        title: 'Ops!',
+        text: "{{ session('error') }}",
+        icon: 'error',
+        confirmButtonText: 'Fechar',
+        customClass: {
+          confirmButton: 'button-red'
+        }
+      });
     });
-  });
   </script>
 @endif
