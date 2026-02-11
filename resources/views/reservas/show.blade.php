09b03071274b2365daae6a70ded2e34eaa2aeeee
Leo S. <contato.prof.leonardo@gmail.com>
2026-02-11 16:40:13 -0400

    Padronização da página

--- resources/views/reservas/show.blade.php
+++ resources/views/reservas/show.blade.php
@@ -1,72 +1,87 @@
 @extends('layouts.app')
+@section('title') {{ 'Detalhes da Reserva' }} @endsection
+@section('content')
 
+<link rel="stylesheet" href="{{ asset('css/main-page.css') }}">
+<link rel="stylesheet" href="{{ asset('css/boxes.css') }}">
 <link rel="stylesheet" href="{{ asset('css/salas.css') }}">
 <link rel="stylesheet" href="{{ asset('css/input-text.css') }}">
 <link rel="stylesheet" href="{{ asset('css/bg.css') }}">
 <link rel="stylesheet" href="{{ asset('css/form-custom.css') }}">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/swal-alert.css') }}">
 
-@section('content')
+@push('scripts')
+  @if(session('success'))
+    <script>
+      document.addEventListener('DOMContentLoaded', function () {
+        Swal.fire({
+          position: 'top',
+          title: 'Sucesso!',
+          text: '{{ session('success') }}',
+          icon: 'success',
+          confirmButtonText: 'Fechar',
+          customClass: {
+            confirmButton: 'button-green'
+          }
+        }).then(() => {
+          window.location.href = "{{ route('home') }}";
+        });
+      });
+    </script>
+  @endif
+@endpush
 
-@section('title') {{ 'Detalhes da Reserva' }} @endsection
+<div class="container pt-5" style="max-width: 480px;">
+  <div class="box-page pb-5">
+    <div class="text-center fw-semibold mb-4">
+      <span class="title-meetings">Detalhes da Reserva</span>
+    </div>
 
-<div class="form-custom no-border-bottom form-no-bottom mt-5">
-  <h5 class="fw-medium text-center text-uppercase">
-    Detalhes da Reserva
-  </h5>
-
-  {{-- <div class="text-center fst-italic" style="color: #374151;">
-    Reserva {{ $reserva->id }}
-  </div> --}}
-
-  <div class="form-line mt-4">
-    <label class="fw-medium">Nome:</label>
-    <span class="input-custom-disabled">
-      {{ $reserva->user->name ?? 'Usuário não encontrado' }}
-    </span>
-  </div>
-
-  <div class="form-line mt-4">
-    <label class="fw-medium">Unidade:</label>
-    <span class="input-custom-disabled">
-      {{ $reserva->user->unidade->nome ?? 'Unidade não encontrada' }}
-    </span>
-  </div>
-
-  <div class="form-line mt-4">
-    <label class="fw-medium">Sala:</label>
-    <span class="input-custom-disabled">
-      {{ $reserva->sala->nome ?? 'Sala não encontrada' }}
-    </span>
-  </div>
-
-  <div class="form-line mt-4">
-    <label class="fw-medium">Data da Reserva:</label>
-
-    <span class="input-custom-disabled">
-      {{ \Carbon\Carbon::parse($reserva->data_inicio)->format('d/m/Y') }}
-    </span>
-  </div>
-
-  <div class="form-line mt-4">
-    <label class="fw-medium">Hora Início / Término:</label>
-    
-    <div class="form-line-split">
-      <div class="input-custom-disabled">
-        {{ \Carbon\Carbon::parse($reserva->data_inicio)->format('H:i') }}
+    <div class="row g-3">
+      <div class="col-12">
+        <label class="fw-medium">Nome:</label>
+        <span class="input-custom-disabled">
+          {{ $reserva->user->name ?? 'Usuário não encontrado' }}
+        </span>        
       </div>
 
-      <div class="input-custom-disabled">
-        {{ \Carbon\Carbon::parse($reserva->data_fim)->format('H:i') }}
+      <div class="col-12">
+        <label class="fw-medium">Unidade:</label>
+        <span class="input-custom-disabled">
+          {{ $reserva->user->unidade->nome ?? 'Unidade não encontrada' }}
+        </span>
+      </div>
+
+      <div class="col-12">
+        <label class="fw-medium">Sala:</label>
+        <span class="input-custom-disabled">
+          {{ $reserva->sala->nome ?? 'Sala não encontrada' }}
+        </span>
+      </div>
+
+      <div class="col-12">
+        <label class="fw-medium">Data da Reserva:</label>
+        <span class="input-custom-disabled">
+          {{ \Carbon\Carbon::parse($reserva->data_inicio)->format('d/m/Y') }}
+        </span>
+      </div>
+
+      <div class="col-12 col-sm-6">
+        <label class="fw-medium">Hora Início:</label>
+        <span class="input-custom-disabled">
+          {{ \Carbon\Carbon::parse($reserva->data_inicio)->format('H:i') }}
+        </span>
+      </div>
+
+      <div class="col-12 col-sm-6">
+        <label class="fw-medium">Hora Término:</label>
+        <span class="input-custom-disabled">
+          {{ \Carbon\Carbon::parse($reserva->data_fim)->format('H:i') }}
+        </span>        
       </div>
     </div>
   </div>
 </div>
 
-<div class="form-custom no-border-top form-no-top pt-3">
-  <div class="d-flex justify-content-end pb-3">
-    <span style="color: #f1f1f1">Jesus vive!</span>
-  </div>
-</div>
 
 @endsection
